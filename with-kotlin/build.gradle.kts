14b7667a0c55e3d43e749f40231eb0c092242356
Daeun Lee <leedaeun.dev@gmail.com>
2025-05-10 21:18:47 +0900

    chore: add Spring(Boot) dependency

--- with-kotlin/build.gradle.kts
+++ with-kotlin/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     kotlin("jvm") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
+    id("org.springframework.boot") version "3.4.3"
+    id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "com.example"
@@ -17,7 +19,7 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.springframework.boot:spring-boot-starter-web")
 }
 
 kotlin {
